0fb2394a4e407deb633aacbc7d5bb39ffba53023
Daniel Delgado <dedd1993@gmail.com>
2019-03-10 18:50:45 -0500

    improvement(carga): avoid double click for saving

--- www/js/controllers/carga.controller.js
+++ www/js/controllers/carga.controller.js
@@ -1,48 +1,35 @@
 angular
     .module('your_app_name.controllers')
-    .controller('CargaCtrl', function ($q, $ionicPopup, $localstorage, $stateParams, BackgroundGeolocationService, $cordovaGeolocation, $ionicLoading, GN7_API_URL, $http, $state, $cordovaNativeStorage, $scope) {
+    .controller('CargaCtrl', function ($q, $ionicPopup, myDateServ, $localstorage, $stateParams, BackgroundGeolocationService, $cordovaGeolocation, $ionicLoading, GN7_API_URL, $http, $state, $cordovaNativeStorage, $scope) {
 		var id = $stateParams.id;
 		$scope.id = $stateParams.id;
 		var envio = {};
 		$scope.form = {};
+		$scope.requestStatusUpdatePending = false;
+
 		$cordovaNativeStorage.getItem('user').then(function (user) {
 			$scope.user = user;
-			function provincias() {
-				return $http.get(GN7_API_URL + 'conductor/envioasignado/' + user.usuario.idconductor);
-			}
-			function distritos() {
-				return $http.get(GN7_API_URL + 'estado_pedido_detalle/PUNTO DE CARGA');
-			}
 			$ionicLoading.show();
 
-			$q.all([,
-				provincias()
-				//,distritos()
-			]).then(function (data) {
-				envio = data[1].data.data;
-				//$scope.opciones = data[2].data.data;
+			$http.get(GN7_API_URL + 'conductor/envioasignado/' + user.usuario.idconductor).then(function (response) {
+				envio = response.data.data;
 				$ionicLoading.show({
 					template: 'Cargando...',
 				});
-				$http.get(GN7_API_URL + 'estado_pedido_detalle/PUNTO DE CARGA/' + envio.pedido_detalles[id].idpedido).then(function (data) {
-					$ionicLoading.hide();
-					$scope.opciones = data.data.data;
-					var idestado_pedido_detalle = 0;
-					//if(envio.pedido_detalles[id].pedido_detalle_estado_pedido_detalles[0].estado_pedido_detalle!=null)
-					if (envio.pedido_detalles[id].pedido_detalle_estado_pedido_detalles[0] != undefined)
-						idestado_pedido_detalle = envio.pedido_detalles[id].pedido_detalle_estado_pedido_detalles[0].estado_pedido_detalle.idestado_pedido_detalle;
-					else
-						idestado_pedido_detalle = $scope.opciones[0].idestado_pedido_detalle - 1;
-					$scope.form.idestado_pedido_detalle = idestado_pedido_detalle;
-					$scope.form.enable = idestado_pedido_detalle + 1;
-				}).catch(function (e) {
-					$ionicLoading.hide();
-					$ionicPopup.alert({
-						template: JSON.stringify(e)
+
+				$http.get(GN7_API_URL + 'estado_pedido_detalle/PUNTO DE CARGA/' + envio.pedido_detalles[id].idpedido)
+					.then(function (data) {
+						$ionicLoading.hide();
+						processEstadosRequest(data);
+
+					}).catch(function (e) {
+						$ionicLoading.hide();
+						$ionicPopup.alert({
+							template: 'Ups... hemos tenido un error'
+						});
 					});
-				});
-				$ionicLoading.hide();
-			}, function () {
+
+			}).catch(function () {
 				$ionicLoading.show({
 					template: 'No se pudo cargar los datos.',
 					duration: 3000
@@ -58,56 +45,40 @@ angular
 		});
 
 		$scope.guardar = function () {
-			var today = new Date();
-			var dd = today.getDate();
-			var mm = today.getMonth() + 1; //January is 0!
-			var yyyy = today.getFullYear();
-			var seconds = today.getSeconds();
-			var minutes = today.getMinutes();
-			var hour = today.getHours();
-			if (dd < 10) {
-				dd = '0' + dd
-			}
-			if (mm < 10) {
-				mm = '0' + mm
-			}
-			if (hour < 10) {
-				hour = '0' + hour
-			}
-			if (minutes < 10) {
-				minutes = '0' + minutes
-			}
-			if (seconds < 10) {
-				seconds = '0' + seconds
-			}
-			today = yyyy + '-' + mm + '-' + dd + ' ' + hour + ':' + minutes + ':' + seconds;
+			$scope.requestStatusUpdatePending = true;
+
 			$scope.form.idpedido_detalle = envio.pedido_detalles[id].idpedido_detalle;
-			$scope.form.fecha = today;
-			$ionicLoading.show({
-				//template: 'Cargando Envío...',
-			});
+			$scope.form.fecha = myDateServ.dateFormat(new Date(), 'yyyy-mm-dd HH:mi:ss');
+			$ionicLoading.show({ });
 			var request = $http({
 				method: "post",
 				url: GN7_API_URL + 'pedido_detalle_estado_pedido_detalle',
 				data: $scope.form
 			});
 			request.then(function (data) {
+				$scope.requestStatusUpdatePending = false;
 				$ionicLoading.hide();
 				if (data.data.status) {
 					$state.go('app.envios');
+				} else {
+					$ionicPopup.alert({ template: "No se pudo guardar"});
 				}
-				else
-					$ionicPopup.alert({
-						template: "No se pudo guardar"
-					});
 			}).catch(function (e) {
+				$scope.requestStatusUpdatePending = false;
 				$ionicLoading.hide();
-				$ionicPopup.alert({
-					template: JSON.stringify(e)
-				});
+				$ionicPopup.alert({ template: JSON.stringify(e)});
 			});
-		}
+		};
 		$scope.cancelar = function () {
 			$state.go('app.estado', { id: id });
-		}
+		};
+
+		var processEstadosRequest = function(data) {
+            $scope.opciones = data.data.data;
+
+            if (envio.pedido_detalles[id].pedido_detalle_estado_pedido_detalles[0] != undefined) {
+				var current_estado_pedido_detalle = envio.pedido_detalles[id].pedido_detalle_estado_pedido_detalles[0].estado_pedido_detalle;
+				$scope.form.idestado_pedido_detalle = current_estado_pedido_detalle.idestado_pedido_detalle;
+			}
+        };
 	})
